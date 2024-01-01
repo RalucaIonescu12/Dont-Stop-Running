5512eb8e364ba5568972143c9d158cd9772554d9
ruxiii <ruxandra.iftimi@gmail.com>
2024-01-01 14:59:48 +0200

    New characters' movement done

--- Assets/Scripts/SwitchAvatar.cs
+++ Assets/Scripts/SwitchAvatar.cs
@@ -14,72 +14,72 @@ public class SwitchAvatar : MonoBehaviour
     {
         avatar1.gameObject.SetActive(true);
         avatar2.gameObject.SetActive(false);
-        //avatar3.gameObject.SetActive(false);
-        //avatar4.gameObject.SetActive(false);
+        avatar3.gameObject.SetActive(false);
+        avatar4.gameObject.SetActive(false);
     }
 
     public void AvatarSwitch()
     {
         switch (whichAvatarIsOn)
         {
+            //case 1:
+            //    whichAvatarIsOn = 2;
+
+            //    avatar1.gameObject.SetActive(false);
+            //    avatar2.gameObject.SetActive(true);
+
+            //    break;
+
+            //case 2:
+            //    whichAvatarIsOn = 1;
+
+            //    avatar1.gameObject.SetActive(true);
+            //    avatar2.gameObject.SetActive(false);
+
+            //    break;
+
+
+
+
             case 1:
                 whichAvatarIsOn = 2;
 
                 avatar1.gameObject.SetActive(false);
                 avatar2.gameObject.SetActive(true);
+                avatar3.gameObject.SetActive(false);
+                avatar4.gameObject.SetActive(false);
 
                 break;
 
             case 2:
+                whichAvatarIsOn = 3;
+
+                avatar1.gameObject.SetActive(false);
+                avatar2.gameObject.SetActive(false);
+                avatar3.gameObject.SetActive(true);
+                avatar4.gameObject.SetActive(false);
+
+                break;
+
+            case 3:
+                whichAvatarIsOn = 4;
+
+                avatar1.gameObject.SetActive(false);
+                avatar2.gameObject.SetActive(false);
+                avatar3.gameObject.SetActive(false);
+                avatar4.gameObject.SetActive(true);
+
+                break;
+
+            case 4:
                 whichAvatarIsOn = 1;
 
                 avatar1.gameObject.SetActive(true);
                 avatar2.gameObject.SetActive(false);
+                avatar3.gameObject.SetActive(false);
+                avatar4.gameObject.SetActive(false);
 
                 break;
-
-
-
-
-                //case 1:
-                //    whichAvatarIsOn = 2;
-
-                //    avatar1.gameObject.SetActive(false);
-                //    avatar2.gameObject.SetActive(true);
-                //    avatar3.gameObject.SetActive(false);
-                //    avatar4.gameObject.SetActive(false);
-
-                //    break;
-
-                //case 2:
-                //    whichAvatarIsOn = 3;
-
-                //    avatar1.gameObject.SetActive(false);
-                //    avatar2.gameObject.SetActive(false);
-                //    avatar3.gameObject.SetActive(true);
-                //    avatar4.gameObject.SetActive(false);
-
-                //    break;
-
-                //case 3:
-                //    whichAvatarIsOn = 4;
-
-                //    avatar1.gameObject.SetActive(false);
-                //    avatar2.gameObject.SetActive(false);
-                //    avatar3.gameObject.SetActive(false);
-                //    avatar4.gameObject.SetActive(true);
-
-                //    break;
-
-                //case 4:
-                //    whichAvatarIsOn = 1;
-
-                //    avatar1.gameObject.SetActive(true);
-                //    avatar2.gameObject.SetActive(false);
-                //    avatar3.gameObject.SetActive(false);
-                //    avatar4.gameObject.SetActive(false);
-
-                //    break;
         }
     }
 
@@ -95,16 +95,16 @@ public class SwitchAvatar : MonoBehaviour
             PrefabUtility.SaveAsPrefabAsset(avatar2, "Assets/Prefab/AvatarPrefab.prefab");
             SceneManager.LoadScene("Game");
         }
-        //else if (whichAvatarIsOn == 3)
-        //{
-        //    PrefabUtility.SaveAsPrefabAsset(avatar3, "Assets/Prefab/AvatarPrefab.prefab");
-        //    SceneManager.LoadScene("Game");
-        //}
-        //else if (whichAvatarIsOn == 4)
-        //{
-        //    PrefabUtility.SaveAsPrefabAsset(avatar4, "Assets/Prefab/AvatarPrefab.prefab");
-        //    SceneManager.LoadScene("Game");
-        //}
+        else if (whichAvatarIsOn == 3)
+        {
+            PrefabUtility.SaveAsPrefabAsset(avatar3, "Assets/Prefab/AvatarPrefab.prefab");
+            SceneManager.LoadScene("Game");
+        }
+        else if (whichAvatarIsOn == 4)
+        {
+            PrefabUtility.SaveAsPrefabAsset(avatar4, "Assets/Prefab/AvatarPrefab.prefab");
+            SceneManager.LoadScene("Game");
+        }
 
     }
 }
